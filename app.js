1e818f2a10e00e26ebb6d0b51855092ff6e9b1b4
Jose Torres & Ashlin Aronin <student@epicodus.com>
2015-09-28 15:08:07 -0700

    Create basic skeleton for index.html and app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var freelancer = angular.module('freelancer', ['ui.router']);
+
+freelancer.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url: '',
+    views: {
+      'header': { templateUrl: 'partials/header.html' },
+      'body':   { templateUrl: 'partials/home.html' },
+      'footer': { templateUrl: 'partials/footer.html'}
+    }
+  });
+
+  // Other states here
+});
